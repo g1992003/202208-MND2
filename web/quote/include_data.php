12afc239cc894a9b7382ae121d42ecf5e724942c
sunnykuo1122 <sunnykuo@mak66design.com>
2023-05-24 18:03:29 +0800

    串接選單資訊

--- web/quote/include_data.php
+++ web/quote/include_data.php
@@ -15,20 +15,31 @@ $title_var = $meta_data[1]["d_text"];
 //關於本部
 $query = "SELECT a_id,a_title,a_title_2 FROM about WHERE a_id > 5 AND a_status = 1 ORDER BY a_order";
 $about_title = sql_data($query, $link, 2, "a_id");
+$about_title_nav = array_chunk($about_title, 10);
 
 //處室分類
 $query = "SELECT dc_id,dc_title,dc_title_2 FROM d_class ORDER BY dc_order";
 $depart_title = sql_data($query, $link, 2, "dc_id");
 $first_depart = reset($depart_title);
+$depart_title_nav = array_chunk($depart_title, 10);
 
 //系統連結
 $query = "SELECT l_id,l_title,l_title_2,l_url,l_type FROM link WHERE l_page = 1 AND l_status = 1 ORDER BY l_order";
-$sys_link_title = sql_data($query, $link, 2, "l_id");
+$sys_link_title = sql_data($query, $link);
+$sys_link_title_nav = $sys_link_title;
+$sys_link_title0 = array(); // 1~8
+foreach ($sys_link_title_nav as $k => $v) {
+    if ($k <= 8) {
+        array_push($sys_link_title0, $v);
+        unset($sys_link_title_nav[$k]);
+    }
+}
+$sys_link_title_nav = array_chunk($sys_link_title_nav, 10);
 
 //好站連結
 $query = "SELECT l_id,l_title,l_title_2,l_url,l_type FROM link WHERE l_page = 2 AND l_status = 1 ORDER BY l_order";
 $link_title = sql_data($query, $link, 2, "l_id");
-
+$link_title_nav = array_chunk($link_title, 10);
 
 //直屬單位
 $query = "SELECT * FROM about WHERE a_id IN (1,2,3,4,5)";
--- web/quote/template/nav.php
+++ web/quote/template/nav.php
@@ -84,94 +84,154 @@ require_once dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'dominator' . DIR
                         <span>關於本部</span>
                         <span>ABOUT US</span>
                     </div>
-                    <ul>
-                        <?php foreach ($about_title as $k => $v) { ?>
-                            <li>
-                                <a href="ediPage.php?id=<?php echo $k; ?>">
-                                    <span><?php echo $v["a_title"]; ?></span>
-                                    <span><?php echo $v["a_title_2"]; ?></span>
-                                </a>
-                            </li>
-                        <?php } ?>
-                    </ul>
+                    <div class="swiper mySwiper1">
+                        <div class="swiper-wrapper">
+                            <?php foreach ($about_title_nav as $k1 => $v1) { ?>
+                                <div class="swiper-slide">
+                                    <ul>
+                                        <?php foreach ($v1 as $k => $v) { ?>
+                                            <li>
+                                                <a href="ediPage.php?id=<?php echo $v["a_id"]; ?>">
+                                                    <span><?php echo $v["a_title"]; ?></span>
+                                                    <span><?php echo $v["a_title_2"]; ?></span>
+                                                </a>
+                                            </li>
+                                        <?php } ?>
+                                    </ul>
+                                <?php } ?>
+                                </div>
+                        </div>
+                        <div class="swiper-button-next"></div>
+                        <div class="swiper-button-prev"></div>
+                    </div>
                 </li>
                 <li class="units">
                     <div class="menu-list">
                         <span>直屬單位</span>
                         <span>UNITS</span>
                     </div>
-                    <ul>
-                        <?php foreach ($article_data as $k => $v) { ?>
-                            <li>
-                                <a href="./?active=<?php echo $k - 1 ?>">
-                                    <span><?php echo $v["a_title"]; ?></span>
-                                    <span><?php echo $v["a_title_2"]; ?></span>
-                                </a>
-                            </li>
-                        <?php } ?>
-                    </ul>
+                    <div class="swiper mySwiper2">
+                        <div class="swiper-wrapper">
+                            <div class="swiper-slide">
+                                <ul>
+                                    <?php foreach ($article_data as $k => $v) { ?>
+                                        <li>
+                                            <a href="./?active=<?php echo $k - 1 ?>">
+                                                <span><?php echo $v["a_title"]; ?></span>
+                                                <span><?php echo $v["a_title_2"]; ?></span>
+                                            </a>
+                                        </li>
+                                    <?php } ?>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="swiper-button-next"></div>
+                        <div class="swiper-button-prev"></div>
+                    </div>
                 </li>
                 <li>
                     <div class="menu-list">
                         <span>處室介紹</span>
                         <span>DEPARTMENTS</span>
                     </div>
-                    <ul>
-                        <?php foreach ($depart_title as $k => $v) { ?>
-                            <li>
-                                <a href="departments.php?id=<?php echo $k; ?>">
-                                    <span><?php echo $v["dc_title"]; ?></span>
-                                    <span><?php echo $v["dc_title_2"]; ?></span>
-                                </a>
-                            </li>
-                        <?php } ?>
-                    </ul>
-                </li>
-                <?php if (!empty($_SESSION["front_account"])) { ?>
-                    <li class="logInShow">
-                        <div class="menu-list">
-                            <span>系統連結</span>
-                            <span>SYSTEMS</span>
-                        </div>
-                        <ul>
-                            <li>
-                                <a href="regulation.php">
-                                    <span>行政規則</span>
-                                    <span>REGULATION</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="policy.php">
-                                    <span>一令到位</span>
-                                    <span>ORDERS</span>
-                                </a>
-                            </li>
-                            <?php foreach ($sys_link_title as $k => $v) { ?>
-                                <li>
-                                    <a <?php echo ($v["l_type"] == 1) ? 'href="' . $v["l_url"] . '" target="_blank" rel="nofollow"' : 'href="edi_system.php?id=' . $k . '"'; ?>>
-                                        <span><?php echo $v["l_title"]; ?></span>
-                                        <span><?php echo $v["l_title_2"]; ?></span>
-                                    </a>
-                                </li>
+                    <div class="swiper mySwiper3">
+                        <div class="swiper-wrapper">
+                            <?php foreach ($depart_title_nav as $k1 => $v1) { ?>
+                                <div class="swiper-slide">
+                                    <ul>
+                                        <?php foreach ($v1 as $k => $v) { ?>
+                                            <li>
+                                                <a href="departments.php?id=<?php echo $v["dc_id"]; ?>">
+                                                    <span><?php echo $v["dc_title"]; ?></span>
+                                                    <span><?php echo $v["dc_title_2"]; ?></span>
+                                                </a>
+                                            </li>
+                                        <?php } ?>
+                                    </ul>
+                                </div>
                             <?php } ?>
-                        </ul>
-                    </li>
-                <?php } ?>
+                        </div>
+                        <div class="swiper-button-next"></div>
+                        <div class="swiper-button-prev"></div>
+                    </div>
+                </li>
+                <?php /*if (!empty($_SESSION["front_account"])) { */ ?>
+                <li class="logInShow">
+                    <div class="menu-list">
+                        <span>系統連結</span>
+                        <span>SYSTEMS</span>
+                    </div>
+                    <div class="swiper mySwiper4">
+                        <div class="swiper-wrapper">
+                            <div class="swiper-slide">
+                                <ul>
+                                    <li>
+                                        <a href="regulation.php">
+                                            <span>行政規則</span>
+                                            <span>REGULATION</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="policy.php">
+                                            <span>一令到位</span>
+                                            <span>ORDERS</span>
+                                        </a>
+                                    </li>
+                                    <?php foreach ($sys_link_title0 as $k => $v) { ?>
+                                        <li>
+                                            <a <?php echo ($v["l_type"] == 1) ? 'href="' . $v["l_url"] . '" target="_blank" rel="nofollow"' : 'href="edi_system.php?id=' . $v["l_id"] . '"'; ?>>
+                                                <span><?php echo $v["l_title"]; ?></span>
+                                                <span><?php echo $v["l_title_2"]; ?></span>
+                                            </a>
+                                        </li>
+                                    <?php } ?>
+                                </ul>
+                            </div>
+                            <?php foreach ($sys_link_title_nav as $k1 => $v1) { ?>
+                                <div class="swiper-slide">
+                                    <ul>
+                                        <?php foreach ($v1 as $k => $v) { ?>
+                                            <li>
+                                                <a <?php echo ($v["l_type"] == 1) ? 'href="' . $v["l_url"] . '" target="_blank" rel="nofollow"' : 'href="edi_system.php?id=' . $v["l_id"] . '"'; ?>>
+                                                    <span><?php echo $v["l_title"]; ?></span>
+                                                    <span><?php echo $v["l_title_2"]; ?></span>
+                                                </a>
+                                            </li>
+                                        <?php } ?>
+                                    </ul>
+                                </div>
+                            <?php } ?>
+                        </div>
+                        <div class="swiper-button-next"></div>
+                        <div class="swiper-button-prev"></div>
+                    </div>
+                </li>
+                <?php /*}*/ ?>
                 <li>
                     <div class="menu-list">
                         <span>好站連結</span>
                         <span>LINKS</span>
                     </div>
-                    <ul>
-                        <?php foreach ($link_title as $k => $v) { ?>
-                            <li>
-                                <a <?php echo ($v["l_type"] == 1) ? 'href="' . $v["l_url"] . '" target="_blank" rel="nofollow"' : 'href="edi_link.php?id=' . $k . '"'; ?>>
-                                    <span><?php echo $v["l_title"]; ?></span>
-                                    <span><?php echo $v["l_title_2"]; ?></span>
-                                </a>
-                            </li>
-                        <?php } ?>
-                    </ul>
+                    <div class="swiper mySwiper5">
+                        <div class="swiper-wrapper">
+                            <?php foreach ($link_title_nav as $k1 => $v1) { ?>
+                                <div class="swiper-slide">
+                                    <ul>
+                                        <?php foreach ($v1 as $k => $v) { ?>
+                                            <li>
+                                                <a <?php echo ($v["l_type"] == 1) ? 'href="' . $v["l_url"] . '" target="_blank" rel="nofollow"' : 'href="edi_link.php?id=' . $v["l_id"] . '"'; ?>>
+                                                    <span><?php echo $v["l_title"]; ?></span>
+                                                    <span><?php echo $v["l_title_2"]; ?></span>
+                                                </a>
+                                            </li>
+                                        <?php } ?>
+                                    </ul>
+                                </div>
+                            <?php } ?>
+                        </div>
+                        <div class="swiper-button-next"></div>
+                        <div class="swiper-button-prev"></div>
+                    </div>
                 </li>
             </ul>
         </nav>
